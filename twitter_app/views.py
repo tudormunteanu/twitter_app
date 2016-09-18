fbeea413491edd56183545ff8c629fc3b100fe09
Edubya77 <ewwatson@asu.edu>
2016-09-18 13:21:20 +0000

    using python-nvd3 wrapper

--- twitter_app/views.py
+++ twitter_app/views.py
@@ -12,6 +12,10 @@ import json
 import pandas as pd
 from . import app
 
+from nvd3 import pieChart
+from collections import Counter
+from collections import defaultdict
+
 @app.route("/")   
 def homepage():
     db_tweet = session.query(Tweet).all()
@@ -22,14 +26,75 @@ def homepage():
     print (len(db_tweet))
     return render_template("base.html",db_tweet=db_tweet, db_tweet_location_decoded=db_tweet_location_decoded)
 
+@app.route("/demo")
+def demo_chart():
+    from nvd3 import pieChart
+
+    # Open File to write the D3 Graph
+    output_file = open('demo.html', 'w')
+
+    type = 'pieChart'
+    chart = pieChart(name=type, color_category='category20c', height=450, width=450)
+    chart.set_containerheader("\n\n<h2>" + type + "</h2>\n\n")
+
+    xdata = ["Orange", "Banana", "Pear", "Kiwi", "Apple", "Strawberry", "Pineapple"]
+    ydata = [3, 4, 0, 1, 5, 7, 3]
+
+    extra_serie = {"tooltip": {"y_start": "", "y_end": " cal"}}
+    chart.add_serie(y=ydata, x=xdata, extra=extra_serie)
+    chart.buildhtml()
+    output_file.write(chart.htmlcontent)
+
+    # close Html file
+    output_file.close()
+
+
+@app.route("/test")
+def create_chart():
+    # Open File to write the D3 Graph
+    output_file = open('output.html', 'w')
+
+    type = 'pieChart'
+    chart = pieChart(name=type, color_category='category20c', height=450, width=450)
+    chart.set_containerheader("\n\n<h2>" + type + "</h2>\n\n")
+
+    x = []
+    xdata = []
+    ydata = []
+    user_language = session.query(Tweet.user_lang)
+    d = defaultdict(int)
+    for language in user_language:
+        d[language] += 1
+    language = list(d.keys())
+    new_list = []
+    for item in language:
+        new_list.append(item[0])
+    count = list(d.values())
+    xdata = new_list
+    ydata = count
+
+    extra_serie = {"tooltip": {"y_start": "", "y_end": " cal"}}
+    chart.add_serie(y=ydata, x=xdata, extra=extra_serie)
+    chart.buildhtml()
+    output_file.write(chart.htmlcontent)
+
+    # close Html file
+    output_file.close()
+    return render_template("test-nvd3.html")
+
 @app.route("/user_lang")
 def user_language():
-    x = []
     user_language = session.query(Tweet.user_lang)
-    for i in user_language:
-        x.append(i)
-    json_data = json.dumps(x)
-    return render_template("user_lang.html", json_data=json_data)
+    d = defaultdict(int)
+    for language in user_language:
+        d[language] += 1
+    lang = list(d.keys())
+    new_list = []
+    for item in lang:
+        new_list.append(item[0])
+    count = list(d.values())
+    print(type(lang))
+    return render_template("user_lang.html", d=d, new_list=new_list, count=count)
 
 @app.route("/location")
 def user_location():
@@ -135,3 +200,4 @@ def login_post():
         return redirect(url_for("login_get"))
     login_user(user)
     return redirect(request.args.get('next') or url_for("entries"))
+
